6ab036d84512ee62517cd6dd65c159ad13e26171
SergeyF <kremlik144@mail.ru>
2023-11-18 12:18:10 +0300

    final editing of fields in the model

--- Django_recipes/recipes/recipesapp/models.py
+++ Django_recipes/recipes/recipesapp/models.py
@@ -11,7 +11,6 @@ class Recipe(models.Model):
     time_for_cooking = models.IntegerField(default=10)
     photo = models.ImageField(upload_to="img/%Y/%m/%d/")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
